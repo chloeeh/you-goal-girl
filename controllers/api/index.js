d0c824a40481c7fd4ad854698a4f0e36b32faa15
LDudrey <113869466+LDudrey@users.noreply.github.com>
2023-01-30 19:51:10 -0600

    fixed login errors

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const profileRoutes = require('./profileRoutes');
 
-router.use('/user', userRoutes);
+router.use('/users', userRoutes);
 router.use('/profile', profileRoutes);
 
 module.exports = router;
